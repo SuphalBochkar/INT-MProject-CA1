ceec63afacb52d432c7a674b3dc3aff8a0a12d7f
Finish66 <suphalbochkar@gmail.com>
2022-10-27 22:04:01 +0530

    M Project

---
+++ My Project/22.py
@@ -0,0 +1,16 @@
+from tkinter import N
+
+Names =    ['Tanishka','Ashish','Vipul','Arpit','Krishna','Vinay','Sidharth','Rishikesh','Anushka','Anmol']
+Marks =    [28,43,31,39,45,21,38,25,15,19]
+Updates =  [-5,+2,-9,-7,+9,-1,-7,-3,-6,+3]
+# Marks.sort()
+
+Names = {'Tanishka':28,'Ashish':43,'Vipul':31,'Arpit':39,'Krishna':45,
+         'Vinay':21,'Sidharth':38,'Rishikesh':25,'Anushka':15,'Anmol':19}
+ls=[]
+
+
+    
+
+
+# N=len(Marks)
