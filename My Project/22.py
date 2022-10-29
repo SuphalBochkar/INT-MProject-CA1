74b775f866ecae39a24392d15da679cdbaadeb5f
Finish66 <suphalbochkar@gmail.com>
2022-10-29 23:52:53 +0530

    Added Dictionary

--- My Project/22.py
+++ My Project/22.py
@@ -1,16 +1,43 @@
-from tkinter import N
-
+# Marks.sort()
+Names1 = {'Tanishka':28,'Ashish':43,'Vipul':31,'Arpit':39,'Krishna':45,
+         'Vinay':21,'Sidharth':38,'Rishikesh':25,'Anushka':15,'Anmol':19}
 Names =    ['Tanishka','Ashish','Vipul','Arpit','Krishna','Vinay','Sidharth','Rishikesh','Anushka','Anmol']
 Marks =    [28,43,31,39,45,21,38,25,15,19]
 Updates =  [-5,+2,-9,-7,+9,-1,-7,-3,-6,+3]
-# Marks.sort()
-
-Names = {'Tanishka':28,'Ashish':43,'Vipul':31,'Arpit':39,'Krishna':45,
-         'Vinay':21,'Sidharth':38,'Rishikesh':25,'Anushka':15,'Anmol':19}
-ls=[]
-
-
+# for i in range(len(Names)):
+#     print(f"Name: {Names[i]} Marks: {Marks[i]} Updates: {Updates[i]}")
     
+    
+for i in range(len(Marks)):
+    print(f"Name: {Names[i]} Marks: {Marks[i]} Updates: {Updates[i]}")
+    
+a={['Tanishka','Ashish','Vipul','Arpit','Krishna','Vinay','Sidharth','Rishikesh','Anushka','Anmol']}
+
+for i in range(10):
+    print(f"{Names1[i]},{Marks[i]} {Updates[i]}")
 
 
-# N=len(Marks)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
