c5b73c30388d5e26ee2a5938a6d1f8a69270fb06
Finish66 <suphalbochkar@gmail.com>
2022-10-31 23:46:33 +0530

    Number b/w Range and adding score

--- Other Projects/2.py
+++ Other Projects/2.py
@@ -1,14 +1,15 @@
-# Create the Rock, Paper and Scissors game with Python, we need to take the user’s choice 
-# and then we need to compare it with the computer choice which is taken using the random module 
-# in Python from a list of choices, and if the user wins, then the score will increase by 1.
-# User: Rock, Paper or Scissors?         User: Rock, Paper or Scissors?
-# CPU: rock                              CPU: paper
-# Tie                                    You win! Paper covers Rock
-# User: Rock, Paper or Scissors?         Rock, Paper or Scissors?
-# CPU: scissors                          End the game
-# You lose… Rock smashes Scissors        Final Scores:
-# CPU:1           Hint: Make use of random module to design the game
-# User:1
+# Rock Paper Scissors
+    # Create the Rock, Paper and Scissors game with Python, we need to take the user’s choice 
+    # and then we need to compare it with the computer choice which is taken using the random module 
+    # in Python from a list of choices, and if the user wins, then the score will increase by 1.
+    # User: Rock, Paper or Scissors?         User: Rock, Paper or Scissors?
+    # CPU: rock                              CPU: paper
+    # Tie                                    You win! Paper covers Rock
+    # User: Rock, Paper or Scissors?         Rock, Paper or Scissors?
+    # CPU: scissors                          End the game
+    # You lose… Rock smashes Scissors        Final Scores:
+    # CPU:1           Hint: Make use of random module to design the game
+    # User:1
 import random
 
 print("Welcome to RPS")
--- Other Projects/3.py
+++ Other Projects/3.py
@@ -1,10 +1,11 @@
-# Email Slicer is just a simple tool that will take multiple email address as an input and slice it to produce the username 
-# and the domain associated with it. The email must be divided into two strings by using ‘@’ as the separator.
-# So, user provides n number of email addresses and you have to design a logic to slice the username 
-# and the domain out of those email addresses. The domain part must print in capitals.
-# Note: Email addresses must be stored first in some container and then operate the required logic on it.
-# EX: abc@gmail.com    xyz@yahoo.com
-# after slicing
-# username :abc and domain: GMAIL.COM    username: xyz and domain: YAHOO.COM
+# Email Slicer
+    # Email Slicer is just a simple tool that will take multiple email address as an input and slice it to produce the username 
+    # and the domain associated with it. The email must be divided into two strings by using ‘@’ as the separator.
+    # So, user provides n number of email addresses and you have to design a logic to slice the username 
+    # and the domain out of those email addresses. The domain part must print in capitals.
+    # Note: Email addresses must be stored first in some container and then operate the required logic on it.
+    # EX: abc@gmail.com    xyz@yahoo.com
+    # after slicing
+    # username :abc and domain: GMAIL.COM    username: xyz and domain: YAHOO.COM
 
 email=str(input("Enter th E-Mail:"))
