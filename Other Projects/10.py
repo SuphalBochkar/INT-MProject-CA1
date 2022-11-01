1e6a129b440c9ba51dc5645b545c650419f4abe7
Finish66 <suphalbochkar@gmail.com>
2022-11-01 23:39:59 +0530

    Scientific Calculator 10

---
+++ Other Projects/10.py
@@ -0,0 +1,7 @@
+# Scientific Calculator
+    # You task is to build a scientific calculator that performs all the below listed functionalities.
+    # Add, sub, multiply, divide, and mod (%) operations on entered integer or floating type numbers.
+    # Square root, exponent (power (a, b))
+    # Sine, cosine, and tangent (Trigonometric functions).
+    # Conversion from radian to degree and degree to radian.
+    # Above listed operations user can perform as many numbers of times until user hits the exit.
