07f3bf1ca980f44a003dcbc8e1ba6a279563c608
Finish66 <suphalbochkar@gmail.com>
2022-11-01 23:44:16 +0530

    Define A Number 11

---
+++ Other Projects/11.py
@@ -0,0 +1,14 @@
+# Define A Number
+    # In this project you have to enter a range [A, B] 
+    # and system will randomly pick any number from your given range and check the status of that given number.
+    # The properties to be checked are:
+    # Is that number is odd or even
+    # Is that number is positive or negative number
+    # Is that number is prime number or composite number.
+    # After checking the status, you have to display all the properties followed by the randomly picked number.
+    # For example
+    # Range is (1,12) and randomly picked number is 10
+    # Then the properties followed by this number are:
+    # 10 is a positive number
+    # 10 is an even number
+    # 10 is a composite number
