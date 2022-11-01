052fef57c5bb00547900b12055b7ece24b479508
Finish66 <suphalbochkar@gmail.com>
2022-11-01 23:30:54 +0530

    Random Stories 7

---
+++ Other Projects/7.py
@@ -0,0 +1,14 @@
+from lib2to3.pgen2.pgen import generate_grammar
+
+
+# Generate Stories 
+    # The task is to generate a random story every time user runs the program.
+    # Every time the user runs the program, we must produce a random tale. 
+    # We'll first store the portions of the tales in distinct lists, and 
+    # then use the Random module to choose random sections of the stories from those lists:
+    # To construct a random narrative, we first imported the random module, 
+    # then built sections of the stories in separate lists, then randomly picked portions of the lists.
+    # Note: You store the portion of your tale/story in different lists, 
+    # and during displaying the story when you pick the portions randomly your complete story must make some sense.
+    # Hint:Random module can be used to select random parts of the story stored in different lists.
+    # Sections of the story can be an adjective, a preposition, a proper noun, etc.
