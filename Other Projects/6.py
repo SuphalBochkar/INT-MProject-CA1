dfc59050e55202fd79510153f0868e641e1db89a
Finish66 <suphalbochkar@gmail.com>
2022-11-01 23:28:27 +0530

    Generate Acronym 6

---
+++ Other Projects/6.py
@@ -0,0 +1,10 @@
+# Generate Acronym
+    # You need to write a python script that generates an acronym word from a given sentence.
+    # Take multiple strings as input in the form of list.
+    # Add the first letter of each string to output.
+    # Iterate over the complete string and add every next letter to space to output.
+    # Change the output to uppercase (required acronym).
+    # You have to generate acronyms for all given strings.
+    # (Student is free to decide the input and output layout for this mini project)
+    # Hint: You can use split and indexing to fetch the first word and then combine it.
+    
