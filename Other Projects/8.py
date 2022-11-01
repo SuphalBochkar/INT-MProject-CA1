87709057f3105697090601a85bbd784ee3a6d303
Finish66 <suphalbochkar@gmail.com>
2022-11-01 23:32:50 +0530

    Currency Converter 8

---
+++ Other Projects/8.py
@@ -0,0 +1,5 @@
+# Currency Converter
+    # Your task is to build a currency converter that will allow you to convert currencies from one unit to another,
+    # such as converting Indian rupee into pounds, euros, US dollar, Canadian dollar, Chinese yuan, Russia’s Rubal, etc.or vice versa.
+    # In this project build a TUI (Text based user interface), where you will enter the source currency to be converted 
+    # and conversion rate. And after conversion display the amount in the target currency.
