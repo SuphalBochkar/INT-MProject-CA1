2cc93b562437a29f4f9ce5955425bce822194825
Finish66 <suphalbochkar@gmail.com>
2022-11-02 22:52:59 +0530

    Fibonacci 12

--- Other Projects/12.py
+++ Other Projects/12.py
@@ -1,8 +1,8 @@
 # Fibonacci Checker
-# In this project user will enter single or multiple numbers 
-# and your system will predict that the entered number or number’s is/are valid number(s) in a Fibonacci series or not.
-# For example, if user inputs 2 numbers
-# 0 and 7
-# 0 is valid and 7 is invalid.
-# Because if we plot Fibonacci series 0 1 1 2 3 5 8 13……, In this series 0 is their but 7 is not present.
-# Constraint: range of the single number or multiple numbers you are entering can be huge.
+    # In this project user will enter single or multiple numbers 
+    # and your system will predict that the entered number or number’s is/are valid number(s) in a Fibonacci series or not.
+    # For example, if user inputs 2 numbers
+    # 0 and 7
+    # 0 is valid and 7 is invalid.
+    # Because if we plot Fibonacci series 0 1 1 2 3 5 8 13……, In this series 0 is their but 7 is not present.
+    # Constraint: range of the single number or multiple numbers you are entering can be huge.
