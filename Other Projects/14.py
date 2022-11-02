eb72328eba894b94a8e0899eb58d3f61e6b06065
Finish66 <suphalbochkar@gmail.com>
2022-11-02 22:56:27 +0530

    Contact Details

---
+++ Other Projects/14.py
@@ -0,0 +1,7 @@
+# Contact Details
+    # You have to create a repository of your classmate’s name and mobile number’s. 
+    # And whenever you want to search the mobile/contact number of some of your classmate,
+    # you can easily search it out from the repository you maintained.
+    # Searching single contact is one of the functionalities, some other functionalities your app must cover are:
+    # Your app must be capable of displaying all the contacts with your classmate names present in the repository.
+    # Your app must be capable of extracting contact numbers of your multiple classmates when required.
