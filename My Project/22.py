012d2a9ba0fb9809f13486a24e5c83b78dfcffa7
Finish66 <suphalbochkar@gmail.com>
2022-11-04 17:16:31 +0530

    Printing values of 2 list

--- My Project/22.py
+++ My Project/22.py
@@ -1,19 +1,22 @@
 # Marks.sort()
-Names1 = {'Tanishka':28,'Ashish':43,'Vipul':31,'Arpit':39,'Krishna':45,
-         'Vinay':21,'Sidharth':38,'Rishikesh':25,'Anushka':15,'Anmol':19}
+# Names1 = {'Tanishka':28,'Ashish':43,'Vipul':31,'Arpit':39,'Krishna':45,
+#          'Vinay':21,'Sidharth':38,'Rishikesh':25,'Anushka':15,'Anmol':19}
 Names =    ['Tanishka','Ashish','Vipul','Arpit','Krishna','Vinay','Sidharth','Rishikesh','Anushka','Anmol']
 Marks =    [28,43,31,39,45,21,38,25,15,19]
-Updates =  [-5,+2,-9,-7,+9,-1,-7,-3,-6,+3]
+# Updates =  [-5,+2,-9,-7,+9,-1,-7,-3,-6,+3]
 # for i in range(len(Names)):
 #     print(f"Name: {Names[i]} Marks: {Marks[i]} Updates: {Updates[i]}")
     
     
-for i in range(len(Marks)):
-    print(f"Name: {Names[i]} Marks: {Marks[i]} Updates: {Updates[i]}")
+# for i in range(len(Marks)):
+#     print(f"Name: {Names[i]} Marks: {Marks[i]} Updates: {Updates[i]}")
     
-a={['Tanishka','Ashish','Vipul','Arpit','Krishna','Vinay','Sidharth','Rishikesh','Anushka','Anmol']}
+for i,j in zip(Names,Marks):
+    print(f'{i} got {j} marks')
+    
+# a={['Tanishka','Ashish','Vipul','Arpit','Krishna','Vinay','Sidharth','Rishikesh','Anushka','Anmol']}
 
-for i in range(10):
-    print(f"{Names1[i]},{Marks[i]} {Updates[i]}")
+# for i in range(10):
+#     print(f"{Names1[i]},{Marks[i]} {Updates[i]}")
     
 
