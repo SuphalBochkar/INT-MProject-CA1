2e71595aa9e9569cc2414059c65ebfde283af32f
Finish66 <suphalbochkar@gmail.com>
2022-11-08 19:17:59 +0530

    No of Days Till born17

---
+++ Other Projects/17.py
@@ -0,0 +1,6 @@
+# No Days Till Now
+    # Design an application where user will input two dates.
+    # His/her date of birth in DD/MM/YY format.
+    # Current (Present day) date in DD/MM/YY format.
+    # Your app will calculate and let the user know how many days that person survived in this world.
+    # Note: Your calculation must be accurate and you have to consider leap and non-leap years separately.
