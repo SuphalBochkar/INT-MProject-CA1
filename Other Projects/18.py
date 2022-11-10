334cd7337d3dcdf817cf3232d06efc4777746259
Finish66 <suphalbochkar@gmail.com>
2022-11-10 22:35:49 +0530

    Clock Angle 18
    Angle between h and m hand

---
+++ Other Projects/18.py
@@ -0,0 +1,9 @@
+# Clock Angle Problem
+    # Given time in hh:mm format in 24-hour notation, calculate the shorter angle between the hour 
+    # and minute hand in an analog clock.
+    # Input: 5:30
+    # Output: 15°
+    # Input: 21:00
+    # Output: 90°
+    # Input: 12:00
+    # Output: 0°
