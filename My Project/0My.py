fdeb1b5e00e71d907e0653450aaf89a8b81476b1
Suphal <suphalbochkar@gmail.com>
2022-11-27 22:25:03 +0530

    Project

---
+++ My Project/0My.py
@@ -0,0 +1,10 @@
+import pprint
+
+# Marks.sort()
+Names1 =    {'Tanishka':28,'Ashish':43,'Vipul':31,'Arpit':39,'Krishna':45,
+            'Vinay':21,'Sidharth':38,'Rishikesh':25,'Anushka':15,'Anmol':19}
+#      a     =    {['Tanishka','Ashish','Vipul','Arpit','Krishna','Vinay','Sidharth','Rishikesh','Anushka','Anmol']}
+Names =    ['Tanishka','Ashish','Vipul','Arpit','Krishna','Vinay','Sidharth','Rishikesh','Anushka','Anmol']
+Marks =    [28,43,31,39,45,21,38,25,15,19]
+Updates =  [-5,+2,-9,-7,+9,-1,-7,-3,-6,+3]
+
