094da777b5375e88fb74a0f5425ce8dc47944e98
Suphal <suphalbochkar@gmail.com>
2022-11-30 20:01:12 +0530

    Update 1m.py

--- My Project/1m.py
+++ My Project/1m.py
@@ -4,3 +4,12 @@ import pprint
 # Names1 =    {'Tanishka':28,'Ashish':43,'Vipul':31,'Arpit':39,'Krishna':45,
 #             'Vinay':21,'Sidharth':38,'Rishikesh':25,'Anushka':15,'Anmol':19}
 #      a     =    {['Tanishka','Ashish','Vipul','Arpit','Krishna','Vinay','Sidharth','Rishikesh','Anushka','Anmol']}
+Names =    ['Tanishka','Ashish','Vipul','Arpit','Krishna','Vinay','Sidharth','Rishikesh','Anushka','Anmol']
+Marks =    [28,43,31,39,45,21,38,25,15,19]
+Updates =  [-5,+2,-9,-7,+9,-1,-7,-3,-6,+3]
+updated_m = []
+d=dict(zip(Names,Marks))
+print(d)
+for i in range(len(Names)):
+    print(f"{i+1}. Name: {Names[i]} Marks: {Marks[i]}")
+    updated_m += [Marks[i] + Updates[i]]
