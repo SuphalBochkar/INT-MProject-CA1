ab07d6037ddeedc6fa1605dd1e05c71f7d7fc335
Suphal <suphalbochkar@gmail.com>
2022-11-30 20:00:59 +0530

    Create 1m.py

---
+++ My Project/1m.py
@@ -0,0 +1,6 @@
+import pprint
+
+# Marks.sort()
+# Names1 =    {'Tanishka':28,'Ashish':43,'Vipul':31,'Arpit':39,'Krishna':45,
+#             'Vinay':21,'Sidharth':38,'Rishikesh':25,'Anushka':15,'Anmol':19}
+#      a     =    {['Tanishka','Ashish','Vipul','Arpit','Krishna','Vinay','Sidharth','Rishikesh','Anushka','Anmol']}
