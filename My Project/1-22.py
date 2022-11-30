ed73c6d216dfe335152e9a40b404472db0b19707
Suphal <suphalbochkar@gmail.com>
2022-11-30 19:59:40 +0530

    MArks list

--- My Project/1-22.py
+++ My Project/1-22.py
@@ -4,3 +4,8 @@ updates=[1,2,3]
 
 # Combine And make dictionary of Names and Marks
 n_m=dict(zip(name,marks))
+
+# Make list of 1..2..3.. to print Ranks
+l=[]
+for j in range(1,len(n_m)+1):
+    l.append(j)
