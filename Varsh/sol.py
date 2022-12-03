acfb0681429cadca865a31c46312a834a88212b5
Suphal <suphalbochkar@gmail.com>
2022-12-03 21:43:52 +0530

    Create sol.py

---
+++ Varsh/sol.py
@@ -0,0 +1,3 @@
+names =    ['Tanishka','Ashish','Vipul','Arpit','Krishna','Vinay','Sidharth','Rishikesh','Anushka','Anmol']
+marks =    [28,43,31,39,28,21,38,25,15,19]
+updates =  [-5,-22,-9,-7,+9,-1,-7,-3,-6,+3]
