d4f3d30156a7d5f70d5dc7d01d07a7c79eeef7ac
Suphal <suphalbochkar@gmail.com>
2022-12-03 21:19:12 +0530

    updated_m

--- My Project/3m.py
+++ My Project/3m.py
@@ -1,3 +1,10 @@
 Names =    ['Tanishka','Ashish  ','Vipul   ','Arpit   ','Krishna ']
 Marks =    [28,43,31,39,45]
 Updates =  [-5,+2,-9,-7,+9]
+    
+
+# print(sorted(d))
+# print(Marks)
+# print(updated_m)
+# print(sorted(updated_m))
+print(dict(d).update(updated_m))
