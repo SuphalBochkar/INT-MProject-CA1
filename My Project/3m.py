051634d90ce4412e51d2197ca09d42b30a7992f8
Suphal <suphalbochkar@gmail.com>
2022-12-03 21:19:31 +0530

    Dictionary

--- My Project/3m.py
+++ My Project/3m.py
@@ -1,6 +1,12 @@
 Names =    ['Tanishka','Ashish  ','Vipul   ','Arpit   ','Krishna ']
 Marks =    [28,43,31,39,45]
 Updates =  [-5,+2,-9,-7,+9]
+print("Rank     Name        Marks")
+updated_m = []
+d=dict(zip(Marks,Names))
+for i in range(len(Names)):
+    print(f"{i+1}        {Names[i]}    {Marks[i]}")
+    updated_m += [Marks[i] + Updates[i]]
     
 
 # print(sorted(d))
