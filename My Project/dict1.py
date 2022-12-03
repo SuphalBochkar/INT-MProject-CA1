7cd600e10d13842f4ac3820773aae5d60ba310bb
Suphal <suphalbochkar@gmail.com>
2022-12-03 21:45:11 +0530

    Soring by key lambda

---
+++ My Project/dict1.py
@@ -0,0 +1,16 @@
+name=input("Enter Student Names: ").split(",")
+marks=map(int,input("Enter Marks: ").split(","))
+
+print("\n")
+
+d=dict(zip(name,marks))
+
+def ranking(d):
+    l=[]
+    for j in range(1,len(d)+1):
+        l.append(j)
+
+    rank=list(sorted(d.items(),key=lambda x:x[1],reverse=True))
+
+    for i in range(len(d)):
+        print("Rank",l[i],":",rank[i][0])
--- Varsh/sol.py
+++ Varsh/sol.py
@@ -1,3 +1,49 @@
 names =    ['Tanishka','Ashish','Vipul','Arpit','Krishna','Vinay','Sidharth','Rishikesh','Anushka','Anmol']
 marks =    [28,43,31,39,28,21,38,25,15,19]
 updates =  [-5,-22,-9,-7,+9,-1,-7,-3,-6,+3]
+a = {}
+ua = {}
+um = []
+for i in range(len(names)):
+    a[names[i]] = marks[i]
+    um += [marks[i]+updates[i]]
+    ua[names[i]] = um[i]
+b = sorted(a.items(), key=lambda k:(k[1], k[0]), reverse=True)
+c = sorted(ua.items(), key=lambda k:(k[1], k[0]), reverse=True)
+
+# ! List Explore
+# print("Old Ranking")
+# for i in range(len(b)):
+#     print(f"{i+1}. Names: {b[i][0]}, Marks: {b[i][1]}")
+# print("New Ranking")
+# for i in range(len(c)):
+#     print(f"{i+1}. Names: {c[i][0]}, Marks: {c[i][1]}")
+
+
+
+
+for i in range(len(b)):
+    if b[i][0] == c[0][0]:
+        print(f"Name: {c[0][0]}, New Marks {c[0][1]}, New Rank: 1, Old Marks {b[i][1]}, Old Rank: {i+1}")
+
+
+for i in range(len(c)):
+    if i != 0:
+        print(f"Rank: {i+1} ")
+        print(f"Names: {c[i][0]}")
+        print(f"Marks: {c[i][1]}")
+    for j in range(len(b)):
+        if c[i][0] in b[j]:
+            if i != 0:
+                print(f"Old rank: {j+1}")
+                print(f"Old Marks: {b[j][1]}")
+            else:
+                print(f"Name: {c[i][0]}, New Marks {c[i][1]}, New Rank: {i+1}, Old Marks {b[j][1]}, Old Rank: {j+1}")
+    print()
+    
+    
+    
+# for i in range(len(b)):
+#     if b[i][0] == c[0][0]:
+#         print(f"{c[0][0]} New: {c[0][1]} Old: {b[i][1]}")
+#         print(f"{c[0][0]} New: 1 Old: {i+1}")
