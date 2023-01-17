233230d93778b15ca7503b9467b7a294cee719cc
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-18 00:38:57 +0530

    Update 2v.py

--- My Project/2v.py
+++ My Project/2v.py
@@ -8,3 +8,18 @@ for i in range (len(names)):
     a[names[i]]=marks[i]
     um=um+[marks[i]+updates[i]]
     ua[names[i]]=um[i]    
+b=sorted(a.items(), key=lambda z:(z[1],z[0]), reverse=True)
+c=sorted(ua.items(), key=lambda z:(z[1],z[0]),reverse=1)
+print(b)
+print(c)
+# !Final Print 
+for i in range(len(c)):
+    print(f"Rank: {i+1} ")
+    print(f"Name: {c[i][0]}")
+    print(f"New Marks: {c[i][1]}")
+    for j in range(len(b)):
+        if c[i][0] == b[j][0]:
+                print(f"Rank Jump: {(j-i)}")
+                print(f"Old rank: {j+1}")
+                print(f"Old Marks: {b[j][1]}")
+    print()
