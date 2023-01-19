bfc13e61c3fd5f849ca2d1f52be0aa334dd442f5
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-20 00:41:37 +0530

    Create f.py

---
+++ My Project/f.py
@@ -0,0 +1,7 @@
+print()
+names = ['Tanishka','Ashish','Vipul','Arpit','Krishna','Vinay','Sidharth','Rishikesh','Anushka','Anmol']
+marks = [91,82,46,67,52,36,47,54,75,34]
+updates = [-26,-12,-20,-14,17,-24,-6,19,-4,8]
+a={}
+um=[]
+ua={}
