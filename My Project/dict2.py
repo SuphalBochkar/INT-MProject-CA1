9df636d86f905cecf62e5aea76b45bf4e2ffb078
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-20 00:40:47 +0530

    Update dict2.py

--- My Project/dict2.py
+++ My Project/dict2.py
@@ -3,3 +3,18 @@ marks=[28,43,31,39,45]
 
 d=dict(zip(name,marks))
 
+def ranking(d):
+    l=[]
+    
+    for j in range(1,len(d)+1):
+        l.append(j)
+        
+    rank=list(sorted(d.items(),key=lambda x:x[1],reverse=True))
+    print(rank)
+    
+    for i in range(len(d)):
+        print("Rank",l[i],":",rank[i][0],"l",rank[i])
+            
+        
+        
+ranking(d)
