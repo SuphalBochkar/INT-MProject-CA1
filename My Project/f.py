b7ff00078090d650ccf4e0b6e2ed3c4c4b61a470
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-23 21:19:40 +0530

    Update f.py

--- My Project/f.py
+++ My Project/f.py
@@ -17,3 +17,19 @@ print(c)
 
 # # !Top Ranker in one row
 
+for i in range(len(b)):
+    if c[0][0] == b[i][0] :
+        print(f"Name: {c[0][0]}, New Marks {c[0][1]}, New Rank: 1, Old Marks {b[i][1]}, Old Rank: {i+1}  Rank Jump: {i}" ) 
+print()
+
+# ! Print All Students 
+for i in range(len(c)):
+    print(f"Rank: {i+1} ")
+    print(f"Name: {c[i][0]}")
+    print(f"New Marks: {c[i][1]}")
+    for j in range(len(b)):
+        if c[i][0] == b[j][0]:
+                print(f"Rank Jump: {(j-i)}")
+                print(f"Old rank: {j+1}")
+                print(f"Old Marks: {b[j][1]}")
+    print()
