cbfeee65d4dfcd55abc144ef085506640ae9620b
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-27 23:39:54 +0530

    Update temp.py

--- My Project/temp.py
+++ My Project/temp.py
@@ -5,3 +5,11 @@ updates = [32,74,-52]
 a={}
 um=[]
 ua={}
+for i in range (len(names)):
+    a[names[i]]=marks[i]
+    um=um+[marks[i]+updates[i]]
+    ua[names[i]]=um[i]    
+b=sorted(a.items(), key=lambda z:(z[1],z[0]), reverse=True)
+c=sorted(ua.items(), key=lambda z:(z[1],z[0]),reverse=1)
+
+# # !Top Ranker in one row
