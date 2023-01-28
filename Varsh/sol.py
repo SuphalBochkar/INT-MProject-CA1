3153fffba025b0fc74903850ea922a76ee4fca83
SuphalBochkar <suphalbochkar@gmail.com>
2023-01-28 23:40:18 +0530

    Delete sol.py

--- Varsh/sol.py
+++ Varsh/sol.py
@@ -1,9 +1,3 @@
-names =    ['Tanishka','Ashish','Vipul','Arpit','Krishna','Vinay','Sidharth','Rishikesh','Anushka','Anmol']
-marks =    [28,43,31,39,28,21,38,25,15,19]
-updates =  [-5,-22,-9,-7,+9,-1,-7,-3,-6,+3]
-a = {}
-ua = {}
-um = []
 for i in range(len(names)):
     a[names[i]] = marks[i]
     um += [marks[i]+updates[i]]
